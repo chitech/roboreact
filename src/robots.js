b85781ffadd2b42c884b20d1e3ba91019e3d1387
Christine Brown <chicagotechpro@gmail.com>
2022-02-15 12:56:20 -0500

    initial commit of completed files

---
+++ src/robots.js
@@ -0,0 +1,54 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Frederick Douglass',
+        email: 'fred@yahoo.com'
+    },
+    {
+        id: 2,
+        name: 'Alex Haley',
+        email: 'ahaley@yahoo.com'
+    },
+    {
+        id: 3,
+        name: 'James Baldwin',
+        email: 'jamesB@yahoo.com'
+    },
+    {
+        id: 4,
+        name: 'Marcus Garvey',
+        email: 'magravey@hotmail.com'
+    },
+    {
+        id: 5,
+        name: 'Pearl Baily',
+        email: 'pearl@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Zora Neal',
+        email: 'Zorn@writeio.com'
+    },
+    {
+        id: 7,
+        name: 'Michelle Obama',
+        email: 'mboma@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Alice Walker',
+        email: 'awalker@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'TaNehesi Coates',
+        email: 'ncoates@hotmail.com'
+    },
+    {
+        id: 10,
+        name: 'Susan Rice',
+        email: 'sreice@hotmail.com'
+    },
+
+
+]
